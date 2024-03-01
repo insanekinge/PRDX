e50ed4b9da450e7999c612725b05ab8ce4fa026f
Vladislav Kinge <VKinge@synergy.ru>
2024-03-01 17:10:01 +0300

    P-2 add about mobile full version

--- js/preloader.js
+++ js/preloader.js
@@ -22,20 +22,21 @@ gsap.to(".preloader", {
     gsap.to(".header", {y: 0, duration: 0.9, ease: "power2.inOut"}); // Плавно выезжаем header сверху на своё место
     gsap.to(".container", {y: 0, duration: 0.9, ease: "power2.inOut", display: "block"}); // Плавно выезжаем container снизу на своё место
     gsap.to(".preloader", {y: 0, duration: 0.9, ease: "power2.inOut"}); 
-    gsap.to(".footer", {y: 0, duration: 0, ease: "power2.inOut", display: "block"}); // Плавно выезжаем preloader снизу на своё место
+    gsap.to(".footer", {y: 0, duration: 0, ease: "power2.inOut", display: "grid"}); // Плавно выезжаем preloader снизу на своё место
     // gsap.set("body, .wrapper", { overflow: "auto" }); 
   }
 });
 
-    let scrollCount = 0; // Переменная для отслеживания количества скролов
-    let Xwidth = '37px';
 
-// Функция для изменения высоты preloader при скролле колёсиком мышки
+let scrollCount = 0; // Переменная для отслеживания количества скролов
+let Xwidth = '37px';
+
+// Функция для изменения высоты preloader при скролле колёсиком мышки или касании на мобильных устройствах
 function handleScroll(event) {
   if (scrollCount < 3) { // Проверка на количество скролов
     gsap.to(".preloader", {
       duration: 0.5, // Продолжительность анимации уменьшения на 20vh
-      height: `-=20vh`, // Уменьшение высоты на 20vh
+      height: `-=25vh`, // Уменьшение высоты на 20vh
       onComplete: () => {
         if (scrollCount === 1) { 
           gsap.to(".preloader__letter:nth-child(2)", { top: "0%",right: "25%", duration: 0.5, ease: "power2.inOut" }); 
@@ -45,9 +46,7 @@ function handleScroll(event) {
 
         if (scrollCount === 2) {
           gsap.to(".preloader__letter:nth-child(2)", { top: "0%", right: "50%", duration: 0.5, ease: "power2.inOut" }); 
-          
           gsap.to(".preloader__letter:nth-child(3)", { bottom: "0%", left: "calc(100% - 37px)", right: "0%",   duration: 0.5, ease: "power2.inOut" }); 
-          
           gsap.to(".preloader__letter:nth-child(4)", { top: "0%", right: "0%", duration: 0.5, ease: "power2.inOut" }); 
         }
 
@@ -55,16 +54,19 @@ function handleScroll(event) {
           gsap.to(".preloader__letter:nth-child(2)", { top: "0%", right: "60%", duration: 0.5, ease: "power2.inOut" }); 
           gsap.to(".preloader__letter:nth-child(3)", { top: "0%",  left: "calc(100% - 37px)", right: "0%",  duration: 0.5, ease: "power2.inOut" }); 
           gsap.to(".preloader__letter:nth-child(4)", {  top: "0%",right: "30%", duration: 0.5, ease: "power2.inOut" }); 
+
+          
         }
 
-
+        
 
         if (scrollCount === 3) {
           document.body.style.overflow = "auto"; // Включение скролла в body после завершения анимации
         } else {
           setTimeout(() => {
             window.addEventListener("wheel", handleScroll, { once: true }); // Добавление задержки между скроллами
-          }, 1000); // Пауза в 1 секунду между скроллами
+            window.addEventListener("touchmove", handleScroll, { once: true }); // Добавление задержки между касаниями
+          }, 500); // Пауза в 0.5 секунду между скроллами
         }
       }
     });
@@ -74,3 +76,4 @@ function handleScroll(event) {
 
 // Добавление слушателя события колёсика мышки для обработки скролла
 window.addEventListener("wheel", handleScroll, { once: true }); // Триггером будет 1 скролл колёсиком мышки
+window.addEventListener("touchmove", handleScroll, { once: true }); // Триггером будет 1 касание на мобильном устройстве
