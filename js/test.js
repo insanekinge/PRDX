3a7ade71ce2ac6a9656b203d17bdb48c887d4543
Vladislav Kinge <VKinge@synergy.ru>
2024-03-20 18:26:01 +0300

    main work on test preloader

--- js/test.js
+++ js/test.js
@@ -8,52 +8,47 @@ function preloaderAnimation() {
   const containerContent = document.getElementById('containerContent');
 
   function handleScroll() {
-    if (containerContent) { // Проверяем наличие элемента
+    if (containerContent) { 
       const containerHeight = container.clientHeight;
       const containerContentTop = containerContent.offsetTop;
       const scrollTop = container.scrollTop;
 
-      // Вычисляем расстояние от верхней границы контейнера до элемента в процентах
+      
       const distance = containerContentTop - scrollTop;
-      const distancePercentage = Math.max(0, (distance / containerHeight) * 100);
+      const distancePercentage = Math.max(0, (distance / containerHeight) * 106); 
 
-      const leftLetter2 = -100 + distancePercentage * 3 <= 0 ? 0 : -100 + (distancePercentage * 3); // Позиция для letter2
+      const leftLetter2 = -100 + distancePercentage * 2 <= 0 ? 0 : -100 + (distancePercentage * 2); 
 
-      const topLetter3 = -100 + distancePercentage * 2 <= 0 ? 0 : -100 + (distancePercentage * 2);
-
-      const leftLetter3 = topLetter3 === 0 ? Math.max(55, 100 - distancePercentage) : 100; // Позиция для letter3
-
-      letter3.style.left = `${leftLetter3}%`;
-      letter3.style.top = `${topLetter3}%`;
-
-      
-      const leftLetter4 = 100 - distancePercentage * 0.7 <= 25 ? 25 : 100 - distancePercentage * 0.7;// Позиция для letter3
-     
-      
-
-      if (leftLetter2 <= 25) {
-        letter2.style.left = `25%`;
+      if (leftLetter2 <= 30) {
+        letter2.style.left = `30%`;
       } else {
         letter2.style.left = `${leftLetter2}%`;
       }
 
-      // if (topLetter3 == 0 ) {
-      //   letter3.style.left = `${leftLetter3}%`;
-      // } else {
-      //   letter3.style.top = `${topLetter3}%`;
-      // }
+      const topLetter3 = -100 + distancePercentage * 2 <= 0 ? 0 : -100 + (distancePercentage * 2);
+      const leftLetter3 = distancePercentage * 2 >= 100 ? 100 : distancePercentage * 2;
 
+      letter3.style.left = `${leftLetter3}%`;
+      letter3.style.top = `${topLetter3}%`;
 
-      // letter3.style.top = `${topLetter3}%`;
+      if (leftLetter3 <= 60) {
+        letter3.style.left = `60%`;
+      } else {
+        letter3.style.left = `${leftLetter3}%`;
+      }
+
+      const leftLetter4 = distancePercentage === 100 ? 100 : 100 - distancePercentage; 
+      const topLetter4 = leftLetter4 >= 86 ? 0 : 100;
 
       letter4.style.left = `${leftLetter4}%`;
+      letter4.style.top = `${topLetter4}%`;
       
 
-      console.log(letter3.style.left);
+      console.log(distancePercentage);
     }
   }
 
-  // Добавляем обработчик события прокрутки к контейнеру
+ 
   container.addEventListener('scroll', handleScroll);
 }
 
