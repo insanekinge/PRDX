ad512d9f61766851f8ae4ad02b02c7a143875ce0
Vladislav Kinge <VKinge@synergy.ru>
2024-03-25 10:30:04 +0300

    main push all sticky styles on prod

--- js/sticky.js
+++ js/sticky.js
@@ -1,8 +1,8 @@
 // Устанавливаем начальные значения для preloader__letter
-gsap.set(".preloader__letter:nth-child(1)", { x: 0, y: 0, top: "45%", left: "45%", transformOrigin: "center center" }); 
-gsap.set(".preloader__letter:nth-child(2)", { x: 0, y: 0, top: "45%", left: "55%", transformOrigin: "center center" }); 
+gsap.set(".preloader__letter:nth-child(1)", { x: 0, y: 0, top: "50%", left: "40%", transformOrigin: "center center" }); 
+gsap.set(".preloader__letter:nth-child(2)", { x: 0, y: 0, top: "50%", left: "55%", transformOrigin: "center center" }); 
 gsap.set(".preloader__letter:nth-child(3)", { x: 0, y: 0, top: "55%", left: "55%", transformOrigin: "center center" }); 
-gsap.set(".preloader__letter:nth-child(4)", { x: 0, y: 0, top: "55%", left: "45%", transformOrigin: "center center" }); 
+gsap.set(".preloader__letter:nth-child(4)", { x: 0, y: 0, top: "55%", left: "40%", transformOrigin: "center center" }); 
 
 // Анимация перемещения preloader__letter в нужные позиции
 gsap.to(".preloader__letter:nth-child(1)", { x: 0, y: 0, top: "0%", left: "0%", duration: 0.7, delay: 0, ease: "power2.inOut" }); 
@@ -34,6 +34,7 @@ gsap.to(".preloader", {
     gsap.to(".container__content", {y: 0, duration: 0.7, ease: "power2.inOut", display: "block"}); 
     gsap.to(".preloader", {y: 0, duration: 0.7, ease: "power2.inOut"}); 
     gsap.to(".footer", {y: 0, duration: 0, ease: "power2.inOut", display: "grid"});
+   
   }
 });
 
@@ -47,43 +48,57 @@ function preloaderAnimation() {
   const containerContent = document.getElementById('containerContent');
 
   function handleScroll() {
-    if (containerContent) { 
+    if (containerContent) {
       const containerHeight = container.clientHeight;
       const containerContentTop = containerContent.offsetTop;
       const scrollTop = container.scrollTop;
-
-      
+  
       const distance = containerContentTop - scrollTop;
-      const distancePercentage = Math.max(0, (distance / containerHeight) * 100); 
-
-      const leftLetter2 = -100 + distancePercentage * 2 <= 0 ? 0 : -100 + (distancePercentage * 2); 
-
+      const distancePercentage = Math.max(0, (distance / containerHeight) * 100);
+  
+      const leftLetter2 = -100 + distancePercentage * 2 <= 0 ? 0 : -87 + distancePercentage * 2;
+  
       if (leftLetter2 <= 30) {
         letter2.style.left = `30%`;
       } else {
         letter2.style.left = `${leftLetter2}%`;
       }
 
-      const topLetter3 = -100 + distancePercentage * 2 <= 2 ? 'var(--headerMarginTop)' : -100 + (distancePercentage * 2) + '%';
+      console.log(leftLetter2);
+  
+      const topLetter3 = -100 + distancePercentage * 2 <= 2 ? 'var(--headerMarginTop)' : -100 + distancePercentage * 2 + '%';
       const leftLetter3 = distancePercentage * 2 >= 100 ? 100 : distancePercentage * 2;
-
+  
       letter3.style.left = `${leftLetter3}%`;
       letter3.style.top = `${topLetter3}`;
-
+  
       if (leftLetter3 <= 60) {
         letter3.style.left = `60%`;
       } else {
         letter3.style.left = `${leftLetter3}%`;
       }
-
-
-      const topLetter4 = -50 + distancePercentage * 2 <= 2 ? 'var(--headerMarginTop)' : -50 + (distancePercentage * 2) + '%';
+  
+      const topLetter4 = -50 + distancePercentage * 2 <= 2 ? 'var(--headerMarginTop)' : -50 + distancePercentage * 2 + '%';
       const leftLetter4 = distancePercentage * 2 >= 187 ? 0 : 175 - distancePercentage * 2;
-      
+  
       letter4.style.left = `${leftLetter4}%`;
       letter4.style.top = `${topLetter4}`;
+  
+      // Проверка на distancePercentage и анимация header
+      if (distancePercentage <= 0) {
+        gsap.to(".header", { marginTop: `calc(- var(--headerMarginTop))`, duration: 0.7, ease: "power2.inOut" });
+      } else {
+        gsap.to(".header", { marginTop: `0`, duration: 0.7, ease: "power2.inOut" });
+      }
+
+      if (distancePercentage > 0) {
+        gsap.to(".header", { marginTop: `0`, duration: 0.7, ease: "power2.inOut" });
+      }
+  
+      // console.log(distancePercentage);
     }
   }
+  
 
  
   container.addEventListener('scroll', handleScroll);
