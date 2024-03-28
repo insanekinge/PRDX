567c5cb322786e5b64042610e973c8fc79ef9ccd
Vladislav Kinge <VKinge@synergy.ru>
2024-03-28 17:30:30 +0300

    main add new technologies preloader

--- js/sticky.js
+++ js/sticky.js
@@ -1,14 +1,14 @@
 // Устанавливаем начальные значения для preloader__letter
-gsap.set(".preloader__letter:nth-child(1)", { x: 0, y: 0, top: "50%", left: "40%", transformOrigin: "center center" }); 
-gsap.set(".preloader__letter:nth-child(2)", { x: 0, y: 0, top: "50%", left: "55%", transformOrigin: "center center" }); 
-gsap.set(".preloader__letter:nth-child(3)", { x: 0, y: 0, top: "55%", left: "55%", transformOrigin: "center center" }); 
-gsap.set(".preloader__letter:nth-child(4)", { x: 0, y: 0, top: "55%", left: "40%", transformOrigin: "center center" }); 
+gsap.set(".preloader__letter:nth-child(1)", { x: 0, y: 0, top: "40%", left: "40%", transformOrigin: "center center" }); 
+gsap.set(".preloader__letter:nth-child(2)", { x: 0, y: 0, top: "40%", left: "60%", transformOrigin: "center center" }); 
+gsap.set(".preloader__letter:nth-child(3)", { x: 0, y: 0, top: "60%", left: "60%", transformOrigin: "center center" }); 
+gsap.set(".preloader__letter:nth-child(4)", { x: 0, y: 0, top: "60%", left: "40%", transformOrigin: "center center" }); 
 
 // Анимация перемещения preloader__letter в нужные позиции
-gsap.to(".preloader__letter:nth-child(1)", { x: 0, y: 0, top: "0%", left: "0%", duration: 0.7, delay: 0, ease: "power2.inOut" }); 
-gsap.to(".preloader__letter:nth-child(2)", { x: 0, y: 0, top: "0%", left: "100%", duration: 0.7, delay: 0, ease: "power2.inOut" }); 
-gsap.to(".preloader__letter:nth-child(3)", { x: 0, y: 0, top: "100%", left: "100%", duration: 0.7, delay: 0, ease: "power2.inOut" }); 
-gsap.to(".preloader__letter:nth-child(4)", { x: 0, y: 0, top: "100%", left: "0%", duration: 0.7, delay: 0, ease: "power2.inOut" });
+gsap.to(".preloader__letter:nth-child(1)", { x: 0, y: 0, top: "0%", left: "0%", duration: 0.3, delay: 0, ease: "power2.inOut" }); 
+gsap.to(".preloader__letter:nth-child(2)", { x: 0, y: 0, top: "0%", left: "100%", duration: 0.3, delay: 0, ease: "power2.inOut" }); 
+gsap.to(".preloader__letter:nth-child(3)", { x: 0, y: 0, top: "100%", left: "100%", duration: 0.3, delay: 0, ease: "power2.inOut" }); 
+gsap.to(".preloader__letter:nth-child(4)", { x: 0, y: 0, top: "100%", left: "0%", duration: 0.3, delay: 0, ease: "power2.inOut" });
 
 // Дополнительная анимация для установки top значения var(--headerMarginTop)
 gsap.to([".preloader__letter:nth-child(1)", ".preloader__letter:nth-child(2)"], { top: "var(--headerMarginTop)", duration: 0.7, delay: 0.7, ease: "power2.inOut" });
@@ -18,22 +18,19 @@ const headerHeight = getComputedStyle(document.documentElement).getPropertyValue
 // Преобразование значения в отрицательное число
 const negativeHeaderHeight = -parseInt(headerHeight, 10);
 
-// Установка отрицательного значения в GSAP
+// // Установка отрицательного значения в GSAP
 gsap.set(".header", {y: negativeHeaderHeight});
 gsap.set(".container", {y: negativeHeaderHeight, height: "100dvh", width: "100%"});
-gsap.set(".container__content", {y: 0, display: "none"}); 
-gsap.set(".footer", {y: 0, display: "none"}); 
-gsap.set(".preloader", {y: "", height: "100dvh", width: "100%"}); 
+gsap.set(".container__content", {y: 0}); 
+gsap.set(".preloader", {y: 0, minHeight: "100dvh", width: "100%"}); 
 gsap.to(".preloader", {
   duration: 0.7, 
   height: "var(--preloaderHeight)",
   ease: "power2.inOut", 
   onComplete: () => { 
     gsap.to(".header", {y: 0, duration: 0.7, ease: "power2.inOut"}); 
-    gsap.to(".container", {y: 0, duration: 0.7, height: "calc(100dvh - var(--headerHeight) + var(--headerMarginTop))",  ease: "power2.inOut", display: "block"}); 
-    gsap.to(".container__content", {y: 0, duration: 0.7, ease: "power2.inOut", display: "block"}); 
-    gsap.to(".preloader", {y: 0, duration: 0.7, ease: "power2.inOut"}); 
-    gsap.to(".footer", {y: 0, duration: 0, ease: "power2.inOut", display: "grid"});
+    gsap.to(".container", {y: 0, duration: 0.7, height: "calc(100dvh - var(--headerHeight))",  ease: "power2.inOut"}); 
+    gsap.to(".preloader", {y: 0, duration: 0.7, minHeight: "var(--preloaderHeight)", ease: "power2.inOut"}); 
    
   }
 });
@@ -56,7 +53,7 @@ function preloaderAnimation() {
       const distance = containerContentTop - scrollTop;
       const distancePercentage = Math.max(0, (distance / containerHeight) * 100);
   
-      const leftLetter2 = -100 + distancePercentage * 2 <= 0 ? 0 : -87 + distancePercentage * 2;
+      const leftLetter2 = -120 + distancePercentage * 3 <= 0 ? 0 : -47 + distancePercentage * 2;
   
       if (leftLetter2 <= 30) {
         letter2.style.left = `30%`;
@@ -66,8 +63,8 @@ function preloaderAnimation() {
 
       console.log(leftLetter2);
   
-      const topLetter3 = -100 + distancePercentage * 2 <= 2 ? 'var(--headerMarginTop)' : -100 + distancePercentage * 2 + '%';
-      const leftLetter3 = distancePercentage * 2 >= 100 ? 100 : distancePercentage * 2;
+      const topLetter3 = -90 + distancePercentage * 2 <= 2 ? 'var(--headerMarginTop)' : -90 + distancePercentage * 2 + '%';
+      const leftLetter3 = distancePercentage * 2 >= 100 ? 110 : distancePercentage * 2;
   
       letter3.style.left = `${leftLetter3}%`;
       letter3.style.top = `${topLetter3}`;
@@ -78,9 +75,10 @@ function preloaderAnimation() {
         letter3.style.left = `${leftLetter3}%`;
       }
   
-      const topLetter4 = -50 + distancePercentage * 2 <= 2 ? 'var(--headerMarginTop)' : -50 + distancePercentage * 2 + '%';
-      const leftLetter4 = distancePercentage * 2 >= 187 ? 0 : 175 - distancePercentage * 2;
-  
+     
+      const leftLetter4 = distancePercentage * 2 >= 180 ? 0 : 150 - distancePercentage * 2;
+      const topLetter4 = 0 + distancePercentage * 2 <= 2 ? 'var(--headerMarginTop)' : 0 + distancePercentage * 2 + '%';
+
       letter4.style.left = `${leftLetter4}%`;
       letter4.style.top = `${topLetter4}`;
   
